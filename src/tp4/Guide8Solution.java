571dfdb623ab8a908a12ea85763d6fb1e9300fcb
tomasberretta <56001082+tomasberretta@users.noreply.github.com>
2020-06-29 17:49:42 -0300

    TP4 terminado

--- src/tp4/Guide8Solution.java
+++ src/tp4/Guide8Solution.java
@@ -49,31 +49,29 @@ public class Guide8Solution implements Guide8 {
     @Override
     public double[] exercise_7(double[][] A, double[] b) {
         int n = b.length;
-        //proceso descendente
-        double aux;
+        double pivot;
         for (int k = 0; k < n ; k++){
-            aux = A[k][k];
+            pivot = A[k][k];
             for (int j = k; j < n; j++ ) {
                 if (j > k + 1) continue;
-                A[k][j] = A[k][j] / aux;
+                A[k][j] = A[k][j] / pivot;
             }
-            b[k] = b[k]/aux;
+            b[k] = b[k]/pivot;
             for (int i = k+1; i < n ; i++){
-                aux = A[i][k];
+                pivot = A[i][k];
                 for (int j = k; j < k+2; j++)
-                    A[i][j]=A[i][j] - aux*A[k][j];
-                b[i]= b[i]-aux*b[k];
+                    A[i][j]=A[i][j] - pivot*A[k][j];
+                b[i]= b[i]-pivot*b[k];
             }
         }
-        // proceso ascendente
         double[] x = new double[n];
-        double suma;
+        double sum;
         x[n-1] = b[n-1];
         for (int i = n-2; i >= 0 ; i--){
-            suma =0;
+            sum =0;
             for (int j = i+1; j < n; j++ )
-                suma = suma + A[i][j]*x[j];
-            x[i]= b[i]-suma;
+                sum = sum + A[i][j]*x[j];
+            x[i]= b[i]-sum;
         }
         return x;
     }
@@ -81,8 +79,56 @@ public class Guide8Solution implements Guide8 {
     // Exercise 9
     @Override
     public double[][] exercise_9(double[][] A) {
-        //todo
-        return null;
+        int n = A.length;
+        double[][] b = new double[n][n*2];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                b[i][j] = A[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n*2; j++) {
+                if(j == (i+n)) b[i][j] = 1;
+            }
+        }
+
+        double[][] temp = new double[b.length][b[0].length];
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                temp[i][j] = b[i][j];
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            double pivot = temp[k][k];
+
+            for (int j = k; j < n*2; j++) {
+                b[k][j] = temp[k][j]/pivot;
+            }
+
+            for (int i = 0; i < n; i++) {
+                if(i == k) continue;
+                for (int j = k; j < n*2; j++) {
+                    b[i][j] = temp[i][j]-temp[i][k]*b[k][j];
+                }
+            }
+
+            for (int i = 0; i < b.length; i++) {
+                for (int j = 0; j < b[i].length; j++) {
+                    temp[i][j] = b[i][j];
+                }
+            }
+        }
+
+        double[][] c = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            int k = 0;
+            for (int j = 0; j < n; j++) {
+                c[i][k] = b[i][j+n];
+                k++;
+            }
+        }
+        return c;
     }
 
     @Override
